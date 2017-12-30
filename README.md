9322895515daae70df9967b90227b8436a37061a
Roman Ch <hello@romangagarin.com>
2017-12-30 09:07:21 +0000

    add project files

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
 # Github Trends
 
-[TODO. There will be project description]
+The program finds repositories on Github created in the previous week, takes 20 high-starred of them and counts the amount of open issues. 
+It outputs the repository name, number of issues and a link to the repo.
+
+# How to launch
+
+The script requires the installed Python interpreter version 3.5
+
+Example of script launch on Linux, Python 3.5:
+```
+$ python github_trending.py
+```
+
+output
+```
+Name: wechat_jump_game
+Issues: 2
+Link: https://api.github.com/repos/wangshub/wechat_jump_game
+
+Name: array-explorer
+Issues: 2
+Link: https://api.github.com/repos/sdras/array-explorer
+
+Name: app-host
+Issues: 2
+Link: https://api.github.com/repos/pluosi/app-host
+
+Name: PS4-4.05-Kernel-Exploit
+Issues: 2
+Link: https://api.github.com/repos/Cryptogenic/PS4-4.05-Kernel-Exploit
+
+Name: javascript-tutorial
+Issues: 2
+Link: https://api.github.com/repos/wangdoc/javascript-tutorial
+
+Name: retrofit2-kotlin-coroutines-adapter
+Issues: 2
+Link: https://api.github.com/repos/JakeWharton/retrofit2-kotlin-coroutines-adapter
+
+
+```
+Running on Windows is similar.
+
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,39 @@
+import requests
+from datetime import datetime, timedelta
+
+top_size = 20
 
 
 def get_trending_repositories(top_size):
-    pass
+    url = 'https://api.github.com/search/repositories'
+    time_now = datetime.now()
+    last_week = (time_now - timedelta(days=7)).strftime('%Y-%m-%d')
+    prepare_date = 'created:>={}'.format(last_week)
+    params = {
+        'q': prepare_date,
+        'sort': 'stars',
+        'order': 'desc',
+        'page': '1',
+        'per_page': top_size
+    }
+    r = requests.get(url, params=params)
+    return r.json()['items']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name)
+    issues = requests.get(url)
+    return len(issues.json())
+
+
+def print_top_github_repo(list_repo):
+    for repo in list_repo:
+        print('Name: {}'.format(repo['name']))
+        print('Issues: {}'.format(get_open_issues_amount(repo['owner']['login'], repo['name'])))
+        print('Link: {}\n'.format(repo['url']))
+
+
 
 if __name__ == '__main__':
-    pass
+    list_top_github_repo = get_trending_repositories(top_size)
+    print_top_github_repo(list_top_github_repo)
