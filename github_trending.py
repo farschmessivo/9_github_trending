b70dd035ad5ee0f5f6ddab5285cc8dcce227ef9b
Roman Ch <hello@romangagarin.com>
2017-12-31 09:40:20 +0000

    change control flow, change var/func names

--- github_trending.py
+++ github_trending.py
@@ -1,16 +1,14 @@
 import requests
 from datetime import datetime, timedelta
 
-top_size = 20
-
 
 def get_trending_repositories(top_size):
     url = 'https://api.github.com/search/repositories'
     time_now = datetime.now()
     last_week = (time_now - timedelta(days=7)).strftime('%Y-%m-%d')
-    prepare_date = 'created:>={}'.format(last_week)
+    created_less_than_week_ago = 'created:>={}'.format(last_week)
     params = {
-        'q': prepare_date,
+        'q': created_less_than_week_ago,
         'sort': 'stars',
         'order': 'desc',
         'page': '1',
@@ -21,18 +19,20 @@ def get_trending_repositories(top_size):
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name)
+    url = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner,
+                                                             repo_name)
     issues = requests.get(url)
     return len(issues.json())
 
 
-def print_top_github_repo(list_repo):
-    for repo in list_repo:
-        print('Name: {}'.format(repo['name']))
-        print('Issues: {}'.format(get_open_issues_amount(repo['owner']['login'], repo['name'])))
-        print('Link: {}\n'.format(repo['url']))
-
-
 if __name__ == '__main__':
-    list_top_github_repo = get_trending_repositories(top_size)
-    print_top_github_repo(list_top_github_repo)
+    top_size = 20
+    top_github_repos = get_trending_repositories(top_size)
+    for repo in top_github_repos:
+        repo_name = repo['name']
+        repo_owner = repo['owner']['login']
+        repo_url = repo['url']
+        print('Name: {}'.format(repo_name))
+        print('Issues: {}'.format(get_open_issues_amount(repo_owner,
+                                                         repo_name)))
+        print('Link: {}\n'.format(repo_url))
